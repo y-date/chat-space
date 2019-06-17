1d4299727c4f857b28e245bdabb8a5961924fa70
Yohei Date <itachi.0101y@gmail.com>
2019-06-17 12:57:13 +0900

    edit-messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,14 +7,15 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Messege.new(messages_params)
+    @message = @group.messages.new(messages_params)
     if @message.save
       redirect_to group_messages_path(@group), notice: "メッセージが送信されました"
     else
-
-
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = "メッセージを入力してください。"
+      render :index
+    end
   end
-
 
   private
   def messages_params
@@ -23,8 +24,5 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
-  end
-
-  def create
   end
 end
