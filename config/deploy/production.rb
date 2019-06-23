452c6e58480c8a7bf938bbdaaa0f567b81359349
Yohei Date <itachi.0101y@gmail.com>
2019-06-23 16:02:54 +0900

    Updata deploy/production.rb just delete ","

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,9 +49,9 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-server "52.194.233.15",
-  user: "ec2-user",
-  roles: %w{app db web},
+server "52.194.233.15", 
+  user: "ec2-user", 
+  roles: %w{app db web}
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
